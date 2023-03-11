1559c1e3aaff01565edb37522005a518a1c95a65
fabiankintero <fabiankintero@gmail.com>
2023-03-11 18:59:28 -0500

    solucion a los ejercicios de js

---
+++ js-1/hello.js
@@ -0,0 +1,8 @@
+function hola(nombre) {
+    return "Hola" + " " + nombre +"!"
+  //return `Hola ${nombre}!` // Me la sugirió javascript
+    } 
+
+console.log(hola("Pedro")) 
+console.log(hola("Juan")) 
+console.log(hola("")) 
---
+++ suma.js
@@ -0,0 +1,14 @@
+
+    function sum(N) {
+
+        let total = 0;
+          for(var i = 1; i <= N; i++){
+            total += i;
+          }
+          return total;
+      }
+      
+
+console.log(sum(4)) // 1 + 2 + 3 + 4 = 10
+console.log(sum(10)) // 55
+console.log(sum(15)) // 120
