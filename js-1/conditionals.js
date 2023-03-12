fd0cf1e70c9d86700779945f6285d1e0e92f2e6b
fabiankintero <fabiankintero@gmail.com>
2023-03-12 12:47:30 -0500

    correcion:condición de mas y +

--- js-1/conditionals.js
+++ js-1/conditionals.js
@@ -1,12 +1,12 @@
 function calcularColor (num) {
-  let color = "";
+  let color = '';
   if (num == 1) {
     color = 'negro';
   } else if (num == 2) {
     color = 'blanco';
   } else if (num == 3) {
     color = 'azul';
-  } else if (num == 8) {
+  } else {
     color = 'verde';
   }
   return 'El color es ' + color;
@@ -16,3 +16,4 @@ console.log (calcularColor (1));
 console.log (calcularColor (2));
 console.log (calcularColor (3));
 console.log (calcularColor (8));
+
--- js-1/hello.js
+++ js-1/hello.js
@@ -1,6 +1,7 @@
-function hola (nombre) {
-  return 'Hola ' + nombre + '!';
+function hola(nombre) {
+  return `Hola ${nombre}!`
 }
+
 
 console.log (hola ('Pedro'));
 console.log (hola ('Juan'));
